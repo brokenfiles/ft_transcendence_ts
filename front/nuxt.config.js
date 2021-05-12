cb5f94464f897b10b7847186020d7fd5d7be9cb4
Louis Laurent <llaurent@student.42.fr>
2021-05-12 14:22:49 +0200

    Fixed socket.io-client types

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -20,7 +20,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/socket.io.js', mode: 'client' }
+    { src: '~/plugins/socket.io.ts', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
