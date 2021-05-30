2cb04a789a3d0f1fb3fd77c6dc3cdf5d084c62ec
Louis Laurent <llaurent@student.42.fr>
2021-05-30 12:15:26 +0200

    Removed useless plugin

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -20,7 +20,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    // { src: '~/plugins/socket.io.ts', mode: 'client' }
     {
       src: '~/plugins/socket.io.extended.js',
       mode: 'client',
